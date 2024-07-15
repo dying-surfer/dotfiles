73f6c094c5d0c01b96fd6ccba71ce53de2ee062a
Dying Surfer <ralph.schneller@gmail.com>
2024-07-15 16:24:10 +0200

    changed install
    Signed-off-by: Dying Surfer <ralph.schneller@gmail.com>

--- install.sh
+++ install.sh
@@ -22,35 +22,12 @@ function install(){
     ln -sf "${full_path}" "${HOME}/${file}"
 }
 
-# Installs a dir (backup & copy)
-function installdir(){
-    dir="$1";
-
-    echo "Backing up $dir"
-    # Backup existing
-    mv "${HOME}/${dir}" "$backup_dir/" > /dev/null 2>&1
-
-    echo "Installing $dir"
-    cp -rs "${dotfiles_dir}/${dir}" "${HOME}/${dir}"
-}
-
-# Used for files that are meant to be overriden
-function customcopy(){
-    file="$1";
-
-    if [[ ! -f "${dotfiles_dir}/custom/${file}" ]]; then
-      cp "${dotfiles_dir}/.bashrc" "${dotfiles_dir}/custom/.bashrc"
-    fi
-}
 
 echo "Creating backup dir ${backup_dir}"
 mkdir "${backup_dir}" || exit 1
 
-#.bashrc is meant to be overriden
-mkdir "${dotfiles_dir}/custom" 2> /dev/null
-customcopy .bashrc
 
-installdir .bash-plugins
+install .bash-plugins
 install .bash_profile
 install .bashrc
 install .dircolors
@@ -60,3 +37,6 @@ install .gitignore
 install .tmux.conf
 install .vimrc
 install .zshrc
+
+
+echo "done installing. make your own local branch to manage custom stuff"
